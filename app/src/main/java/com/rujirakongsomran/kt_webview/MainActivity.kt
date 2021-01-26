f22acfa40afff00ac0cd9f4fe2c531035c5920e1
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-01-26 15:22:52 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_webview/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_webview/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.rujirakongsomran.kt_webview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.rujirakongsomran.kt_webview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        webViewSetup()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun webViewSetup() {
+
+        // Handling page navigation
+        binding.wv.webViewClient = WebViewClient()
+        binding.wv.apply {
+            binding.wv.loadUrl("http://www.google.com")
+            // Enabling JavaScript
+            binding.wv.settings.javaScriptEnabled = true
+            binding.wv.settings.safeBrowsingEnabled = true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.wv.canGoBack()) binding.wv.goBack() else super.onBackPressed()
     }
 }
